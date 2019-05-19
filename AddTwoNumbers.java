04fc4c112d8cfdb0a7a35f3bde61e56fe2fbafed
Bader Barbarawi <bad1996.2014@gmail.com>
2019-05-19 14:22:56 +0200

    Added main file that prints (5+8) total

---
+++ AddTwoNumbers.java
@@ -0,0 +1,12 @@
+public class AddTwoNumbers {
+
+  public static void main(String[] args) {
+
+      System.out.println("AddTwoNumbers");
+
+      double total = 0;
+      total = 5 + 8;
+      
+      System.out.print("Der Summe von (5+8) ist = " + total);
+  }
+}
